c60f80800287644e0b98fd72196507cbce762a96
apple <seul.dos@gmail.com>
2021-08-27 19:06:16 +0800

    feat(spring-puzzles): spring jdbc about init

---
+++ spring-jdbc/src/main/java/org/example/jdbc/JdbcApplication.java
@@ -0,0 +1,15 @@
+package org.example.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author renc
+ */
+@SpringBootApplication
+public class JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcApplication.class, args);
+    }
+}
--- spring-quartz/src/main/java/org/example/quartz/schedule/listener/QuartzSchedulerListener.java
+++ spring-quartz/src/main/java/org/example/quartz/schedule/listener/QuartzSchedulerListener.java
@@ -1,5 +1,6 @@
 package org.example.quartz.schedule.listener;
 
+import org.quartz.*;
 import org.quartz.listeners.SchedulerListenerSupport;
 import org.springframework.stereotype.Component;
 
@@ -8,4 +9,104 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class QuartzSchedulerListener extends SchedulerListenerSupport {
+
+    @Override
+    public void jobAdded(JobDetail jobDetail) {
+        super.jobAdded(jobDetail);
+    }
+
+    @Override
+    public void jobDeleted(JobKey jobKey) {
+        super.jobDeleted(jobKey);
+    }
+
+    @Override
+    public void jobPaused(JobKey jobKey) {
+        super.jobPaused(jobKey);
+    }
+
+    @Override
+    public void jobResumed(JobKey jobKey) {
+        super.jobResumed(jobKey);
+    }
+
+    @Override
+    public void jobScheduled(Trigger trigger) {
+        super.jobScheduled(trigger);
+    }
+
+    @Override
+    public void jobsPaused(String jobGroup) {
+        super.jobsPaused(jobGroup);
+    }
+
+    @Override
+    public void jobsResumed(String jobGroup) {
+        super.jobsResumed(jobGroup);
+    }
+
+    @Override
+    public void jobUnscheduled(TriggerKey triggerKey) {
+        super.jobUnscheduled(triggerKey);
+    }
+
+    @Override
+    public void schedulerError(String msg, SchedulerException cause) {
+        super.schedulerError(msg, cause);
+    }
+
+    @Override
+    public void schedulerInStandbyMode() {
+        super.schedulerInStandbyMode();
+    }
+
+    @Override
+    public void schedulerShutdown() {
+        super.schedulerShutdown();
+    }
+
+    @Override
+    public void schedulerShuttingdown() {
+        super.schedulerShuttingdown();
+    }
+
+    @Override
+    public void schedulerStarted() {
+        super.schedulerStarted();
+    }
+
+    @Override
+    public void schedulerStarting() {
+        super.schedulerStarting();
+    }
+
+    @Override
+    public void triggerFinalized(Trigger trigger) {
+        super.triggerFinalized(trigger);
+    }
+
+    @Override
+    public void triggerPaused(TriggerKey triggerKey) {
+        super.triggerPaused(triggerKey);
+    }
+
+    @Override
+    public void triggerResumed(TriggerKey triggerKey) {
+        super.triggerResumed(triggerKey);
+    }
+
+    @Override
+    public void triggersPaused(String triggerGroup) {
+        super.triggersPaused(triggerGroup);
+    }
+
+    @Override
+    public void triggersResumed(String triggerGroup) {
+        super.triggersResumed(triggerGroup);
+    }
+
+    @Override
+    public void schedulingDataCleared() {
+        super.schedulingDataCleared();
+    }
 }
